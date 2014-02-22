6f5ca9005073a202fc96d2bb534bc9f07ebffae3
Lumir Balhar <balhar@rpi.frenzy.cz>
2014-02-22 16:21:42 +0100

    Snizeno rozliseni pro zmenseni rozmeru fotografii a nastaven gitignore na porizene fotografie.

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import time
 now = time.strftime('%Y-%m-%d_%H:%M:%S')
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (2592, 1944)
+    camera.resolution = (1440, 960)
     camera.capture('./images/' + now + '.jpg')
 
