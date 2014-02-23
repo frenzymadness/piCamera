b6ea13268d1da86fd120dd89e9d244ad36d5d389
Lumir Balhar <balhar@rpi.frenzy.cz>
2014-02-23 22:03:37 +0100

    S ohledem na prenos, ukladani z obrazeni dat bylo znovu snizeno rozliseni porizovanych fotografii.

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import time
 now = time.strftime('%Y-%m-%d_%H:%M:%S')
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1440, 960)
+    camera.resolution = (720, 480)
     camera.capture('./images/' + now + '.jpg')
 
